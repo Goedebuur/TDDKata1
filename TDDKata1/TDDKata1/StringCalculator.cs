5f3b90387f069125bf70848d2961d8dd4bcb2e3f
Goedebuur <Arjen.Goedegebure@gmail.com>
2014-09-01 14:15:20 +0200

    Empty string test

---
+++ TDDKata1/TDDKata1/StringCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace TDDKata1
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ TDDKata1/TDDKata1/StringCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace TDDKata1
+{
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            int result = stringCalculator.Add(string.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
